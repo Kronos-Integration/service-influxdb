3d15e4b135e60ec76b152f20e7b7a5a2de990f1c
Markus Felten <Markus.Felten@gmx.de>
2025-07-24 21:55:29 +0100

    fix: remove debug output

--- src/point-endpoint.mjs
+++ src/point-endpoint.mjs
@@ -4,13 +4,8 @@ import { ReceiveEndpoint } from "@kronos-integration/endpoint";
 export class PointEndpoint extends ReceiveEndpoint {
   async receive(value) {
     const num = Number.parseFloat(value.toString());
-
     const [pointName, fieldName] = this.name.split(".");
-
-    console.log(pointName, fieldName, num);
-
     const point = new Point(pointName).floatField(fieldName, num);
-
     this.owner.writeApi.writePoint(point);
   }
 }
